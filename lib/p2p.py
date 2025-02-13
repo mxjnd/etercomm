50d4d54982ed6debdd14fc74fd8b22c61465c8d1
mxjnd <166603997+mxjnd@users.noreply.github.com>
2025-02-13 17:01:07 +0100

    Update p2p.py

--- lib/p2p.py
+++ lib/p2p.py
@@ -1,28 +1,23 @@
-import threading
 import socket
 
-class Client:
-    def __init__(self, addr, port):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.bind((addr, port))
+class Peer:
+    def __init__(self):
+        self.timeout = 0
         self.buffsize = 2048
+        self.encoding = 'utf-8'
     def send_data(self, conn, data):
-        conn.sendall(data.encode('utf-8'))
+        conn.sendall(data.encode(self.encoding))
     def recv_data(self, conn):
-        while True:
-          return conn.recv(self.buffsize).decode('utf-8')
-    def start_listen(self):
-        self.client.listen(1)
-        conn, addr = self.client.accept()
-        threading.Thread(target=self.send_data, args=(conn, 'ciao')).start()
-        threading.Thread(target=self.recv_data, args=(conn,)).start()
-    def start_connect(self, addr, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((addr, port))
-        threading.Thread(target=self.send_data, args=(sock, 'ciao')).start()
-        threading.Thread(target=self.recv_data, args=(sock,)).start()
+        return conn.recv(self.buffsize).decode(self.encoding)
+    def listen_at(self, addr, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.bind((addr, port))
+        listener.listen(1)
+        return listener.accept()
+    def connect_to(self, addr, port):
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.connect((addr, port))
+        return connection
 
 if __name__ == '__main__':
-    client = Client('192.168.1.53', 4444)
-    client.start_listen()
-    threading.Thread(target=client.start_connect, args=('192.168.1.21', 4444)).start() 
+    pass
