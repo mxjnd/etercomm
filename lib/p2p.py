5954f762d8658d4d7771d851466b4e3b2a267c1a
mxjnd <166603997+mxjnd@users.noreply.github.com>
2025-02-13 17:49:38 +0100

    Update p2p.py

--- lib/p2p.py
+++ lib/p2p.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 from threading import Thread
 
 class Peer:
@@ -14,11 +15,16 @@ class Peer:
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.bind((addr, port))
         listener.listen(1)
-        return listener.accept()
+        contex = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        contex.load_cert_chain(keyfile=r'C:\Users\salvo\Documents\py\etercomm\etercomm\lib\key.pem', certfile=r'C:\Users\salvo\Documents\py\etercomm\etercomm\lib\cert.pem')
+        conn, addr = listener.accept()
+        tls = contex.wrap_socket(conn)
     def connect_to(self, addr, port):
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection.connect((addr, port))
-        return connection
+        connection = socket.create_connection((addr, port))
+        contex = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        contex.load_cert_chain(keyfile=r'C:\Users\salvo\Documents\py\etercomm\etercomm\lib\key.pem', certfile=r'C:\Users\salvo\Documents\py\etercomm\etercomm\lib\cert.pem')
+        tls = contex.wrap_socket(connection, addr)
+        return tls
 
 if __name__ == '__main__':
     peer = Peer()
