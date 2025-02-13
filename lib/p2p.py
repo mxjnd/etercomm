46c61a6e6218fd5def2004ef28cb74175882710b
mxjnd <166603997+mxjnd@users.noreply.github.com>
2025-02-13 11:36:54 +0100

    Update p2p.py

--- lib/p2p.py
+++ lib/p2p.py
@@ -1,7 +1,19 @@
+import socket
+
 class Client:
-  def __init__(self):
+  def __init__(self, addr, port):
+    self.client = socket.socket(socket.SOCK_STREAM, socket.AF_INET)
+    self.client.bind((addr, port))
+  def connto(self, addr, port):
+    self.client.connect((addr, port))
+  def sendata(self, data):
     pass
     
 class Server:
-  def __init__(self):
+  def __init__(self, addr, port):
+    self.server = socket.socket(socket.SOCK_STREAM, socket.AF_INET)
+    self.client.bind((addr, port))
+  def handle_newconn(self):
+    pass
+  def handle_recvdata(self, data):
     pass
